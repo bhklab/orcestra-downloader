fbc76178a83c51ed7e046fa3f200f7446be975b9
Jermiah Joseph <44614774+jjjermiah@users.noreply.github.com>
2025-05-08 15:39:35 -0400

    feat: Add clinical genomics datasets  (#7)
    * feat: Add clinical genomics dataset configuration
    
    * chore: update lockfile
    
    * feat: Add ClinicalGenomics model and update dataset configuration
    
    * chore(sem-ver): bump version to 0.11.2 and update sha256
    
    * style: Format dataset configuration for consistency

--- src/orcestradownloader/models/__init__.py
+++ src/orcestradownloader/models/__init__.py
@@ -4,5 +4,14 @@ from .radiomicset import RadiomicSet
 from .radioset import RadioSet
 from .toxicoset import ToxicoSet
 from .xevaset import XevaSet
+from .clinicalgenomics import ClinicalGenomics
 
-__all__ = ['ICBSet', 'PharmacoSet', 'RadioSet', 'ToxicoSet', 'XevaSet', 'RadiomicSet']
+__all__ = [
+    "ICBSet",
+    "PharmacoSet",
+    "RadioSet",
+    "ToxicoSet",
+    "XevaSet",
+    "RadiomicSet",
+    "ClinicalGenomics",
+]
